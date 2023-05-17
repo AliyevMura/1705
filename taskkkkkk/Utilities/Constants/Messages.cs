bdc86286bb044febc4f94ac15e1153f92020c14e
AliyevMura <7lvghl9@code.edu.az>
2023-05-17 11:33:08 +0400

    initial commit

---
+++ taskkkkkk/Utilities/Constants/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace WebFrontToBack.Utilities.Constants
+{
+    public static class Messages
+    {
+        public static string FileTypeMustBeImage = "file  type must be image type";
+        public static string FileTypeMustBeText = "file  type must be text type";
+    }
+}
